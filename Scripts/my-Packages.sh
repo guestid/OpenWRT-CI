18f78ae1a54e536433186cf9fee66f04f3d5f06e
1 <1>
2025-09-15 13:47:58 +0800

    1

--- Scripts/my-Packages.sh
+++ Scripts/my-Packages.sh
@@ -115,7 +115,7 @@ UPDATE_VERSION() {
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
-UPDATE_VERSION "tailscale"
+#UPDATE_VERSION "tailscale"
 
 #########################
 UPDATE_PACKAGE "luci-app-taskplan" "sirpdboy/luci-app-taskplan" "master"
